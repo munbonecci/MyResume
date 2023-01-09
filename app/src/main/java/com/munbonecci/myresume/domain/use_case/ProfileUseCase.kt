92342f26a1e844f4940b6b84ef4a53dc0f0617b5
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2023-01-09 16:53:22 -0600

    flow/catch added instead try/catch

--- app/src/main/java/com/munbonecci/myresume/domain/use_case/ProfileUseCase.kt
+++ app/src/main/java/com/munbonecci/myresume/domain/use_case/ProfileUseCase.kt
@@ -6,21 +6,17 @@ import com.munbonecci.myresume.data.model.ProfileData
 import com.munbonecci.myresume.domain.DataGenerator
 import com.munbonecci.myresume.domain.use_case.ProfileConstants.ERROR_CODE_SERVICE
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-import java.io.IOException
 import javax.inject.Inject
 
 
 class ProfileUseCase @Inject constructor(private val context: Context) {
     operator fun invoke(): Flow<Resource<ProfileData>> = flow {
-        try {
-            emit(Resource.Loading())
-            emit(Resource.Success(DataGenerator(context).profileData))
-        } catch (e: IOException) {
-            emit(Resource.Error(ERROR_CODE_SERVICE))
-        } catch (e: Exception) {
-            emit(Resource.Error(ERROR_CODE_SERVICE))
-        }
+        emit(Resource.Loading())
+        emit(Resource.Success(DataGenerator(context).profileData))
+    }.catch {
+        emit(Resource.Error(ERROR_CODE_SERVICE))
     }
 }
 
